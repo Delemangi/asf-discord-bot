1d90575cd7624ec6fb8f49e1babe838119688da6
Delemangi <stefan@heckgaming.com>
2021-11-25 11:17:11 +0100

    add readme and config example

---
+++ readme.md
@@ -0,0 +1,7 @@
+# HECKGAMING Discord bot
+
+## Running
+ - `npm install`
+ - Modify `config.example.ts` to your liking and rename it to `config.ts`
+ - `npm run build`
+ - `npm run run`
---
+++ src/config.example.ts
@@ -0,0 +1,30 @@
+export const config = {
+    token: '',
+    clientID: '',
+    inviteURL: '',
+    asfAPI: '',
+    asfWS: '',
+    alphaVantageAPI: '',
+    asfChannels: [
+        ''
+    ],
+    asfLogChannels: [
+        ''
+    ],
+    rustChannels: [
+        ''
+    ],
+    guildIDs: [
+        ''
+    ],
+    mails: [
+        {
+            'server': '',
+            'ssl': false,
+            'email': '',
+            'emailPassword': '',
+            'emailFolder': ''
+        }
+    ],
+    asfPermissions: {}
+};
